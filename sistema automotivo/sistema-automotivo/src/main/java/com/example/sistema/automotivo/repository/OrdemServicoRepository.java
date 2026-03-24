3e9cc47259fd11fe687fa3b47d8848385ade1bbf
Lucas Gabriel <lucasgabriel19672@gmail.com>
2026-03-24 16:13:48 -0300

    Delete sistema automotivo/sistema-automotivo directory

--- sistema automotivo/sistema-automotivo/src/main/java/com/example/sistema/automotivo/repository/OrdemServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema.automotivo.repository;
-
-import com.example.sistema.automotivo.model.OrdemServicoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdemServicoRepository extends JpaRepository<OrdemServicoModel, Long> {
-}
